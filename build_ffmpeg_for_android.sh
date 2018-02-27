423ed01827aadecdbe6418745345ecfc033e850c
Vadim Gusis <vadim.gusis@gmail.com>
2018-02-27 17:28:06 +0200

    jni+mediacodec enabled

--- build_ffmpeg_for_android.sh
+++ build_ffmpeg_for_android.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-SYSROOT=$ANDROID_HOME/ndk-bundle/platforms/android-23/arch-arm/
-TOOLCHAIN=$ANDROID_HOME/ndk-bundle/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/
+NDK_BUNDLE=$ANDROID_HOME/ndk-bundle-bak-14.1
+SYSROOT=$NDK_BUNDLE/platforms/android-23/arch-arm/
+TOOLCHAIN=$NDK_BUNDLE/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/
 ANDROID_INCLUDE=${SYSROOT}usr/include/
 
 # Original URL: git://source.ffmpeg.org/ffmpeg.git
@@ -22,18 +23,25 @@ function build_one
     --disable-doc \
     --disable-symver \
     --cross-prefix=${TOOLCHAIN}bin/arm-linux-androideabi- \
-    --target-os=linux \
+    --target-os=android \
     --arch=arm \
     --enable-cross-compile \
+    --enable-jni \
+    --enable-mediacodec \
+    --enable-decoder=h264_mediacodec \
+    --enable-hwaccel=h264_mediacodec \
+    --enable-neon \
     --sysroot=$SYSROOT \
-    --extra-cflags="-Os -fpic $ADDI_CFLAGS" \
+    --extra-cflags="$ADDI_CFLAGS $EXTRA_CFLAGS -march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16 " \
     --extra-ldflags="$ADDI_LDFLAGS" \
     $ADDITIONAL_CONFIGURE_FLAG
 make clean
 make -j8
 make install
 }
-CPU=arm
+CPU=arm-v7a
 PREFIX=$(pwd)/android/$CPU
-ADDI_CFLAGS="-marm -I$ANDROID_INCLUDE -DANDROID_MULTINETWORK=1"
+
+ADDI_CFLAGS="-DANDROID -marm -I$ANDROID_INCLUDE -DANDROID_MULTINETWORK=1"
+EXTRA_CFLAGS="-fPIC -ffunction-sections -funwind-tables -fstack-protector -fomit-frame-pointer -fstrict-aliasing -funswitch-loops -finline-limit=300"
 build_one
